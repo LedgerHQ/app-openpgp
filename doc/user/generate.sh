942e16f74ab8aa71644be8e6780ef63109cba599
François Colas <francois.colas@ledger.fr>
2020-07-10 11:19:31 +0200

    Fix corrupted PDF file

--- doc/user/generate.sh
+++ doc/user/generate.sh
@@ -2,8 +2,8 @@
 
 NAME=blue-app-openpgp-card
 
-rm -f ${NAME}.pdf ${NAME}.latex
+rm -f ${NAME}.pdf
 
-OUTPUT_FORMAT=rst
+OUTPUT_FORMAT=rst+smart
 
 pandoc -s --template=${NAME}.template -f ${OUTPUT_FORMAT} -V geometry:a4paper -V geometry:margin=1in -V fontsize=10pt -t latex --toc -N -o ${NAME}.pdf ${NAME}.rst
